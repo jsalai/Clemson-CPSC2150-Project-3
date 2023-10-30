8f680ed97707af7c44955f0ee19966beaf067fd3
jsalai <124062102+jsalai@users.noreply.github.com>
2023-10-29 21:34:43 -0400

    Update GameBoardMem.java

--- cpsc2150/extendedConnectX/models/GameBoardMem.java
+++ cpsc2150/extendedConnectX/models/GameBoardMem.java
@@ -15,19 +15,38 @@ public class GameBoardMem extends AbsGameBoard {
         numToWin = win;
     }
 
+    /**
+     * Function to return number of rows on board (length)
+     * @return row number
+     */
     public int getNumRows() {
         return numRows;
     }
 
+    /**
+     * Function to return number of columns on board (width)
+     * @return column number
+     */
     public int getNumColumns() {
         return numCols;
     }
 
+    /**
+     * Function to return number of token to be placed in a row in order for game to result in a win
+     * @return number to win
+     */
     public int getNumToWin() {
         return numToWin;
     }
 
-    public void placeToken(char p, int c) {
+    /**
+     * Function to place specific character token in given position
+     * @param p is the token to be placed on the game board
+     * @param c is the column in which the token will be placed
+     * @pre p is existing character token, c is a valid column in which a token can be placed
+     * @post given character token will be placed in specified column
+     */
+    public void dropToken(char p, int c) {
         if(mapBoard.containsKey(p) == false) {
             mapBoard.put(p, new ArrayList<>());
         }else {
@@ -41,6 +60,12 @@ public class GameBoardMem extends AbsGameBoard {
         }
     }
 
+    /**
+     * Function to determine what is at the given position
+     * @param pos is a BoardPosition object that gives the coordinates of a cell
+     * @post will return token of a player, or ' '
+     * @return the character token of a player at position, or ' ' if blank
+     */
     public char whatsAtPos(BoardPosition pos) {
         for(HashMap.Entry<Character, List<BoardPosition>> map : mapBoard.entrySet()) {
             if(isPlayerAtPos(pos, map.getKey())) {
@@ -50,6 +75,14 @@ public class GameBoardMem extends AbsGameBoard {
         return ' ';
     }
 
+    /**
+     * Overridden function to determine if the player is at the given position
+     * @param pos is a position in grid
+     * @param p is the character token of the player
+     * @pre character must be existing token character, and pos is of format (row, col)
+     * @post true iff pos on board is available, false otherwise
+     * @return true if player is at given position, false otherwise
+     */
     @Override
     public boolean isPlayerAtPos(BoardPosition pos, char p) {
         if(mapBoard.containsKey(p)) {
